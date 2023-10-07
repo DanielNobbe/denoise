f8bbf289c0beff60af84c4b0b1b5e76dcfed379b
Daniel Nobbe <daniellnobbe@gmail.com>
2023-10-07 13:30:25 +0200

    added sampling of sensitivity to poisson

--- models/noise.py
+++ models/noise.py
@@ -7,18 +7,36 @@ import os
 
 
 class ShotNoise:
-    def __init__(self, pixel_sensitivity=0.1):
-        self.pixel_sensitivity = pixel_sensitivity
+    def __init__(self, sensitivity=0.1, sensitivity_sigma=None, max_sens=0.1, min_sens=0.0):
+        # if sensitivity_sigma is defined, will sample the 
+        # sensitivity from a lognormal at every image 
+        self.pixel_sensitivity = sensitivity
         self.rng = torch.Generator()
+
+        if sensitivity_sigma is not None:
+            self.distr = torch.distributions.LogNormal(sensitivity, sensitivity_sigma)
+            self.max_sens = max_sens
+            self.min_sens = min_sens
+        else:
+            self.distr = None
+        
 
     def __call__(self, img: torch.Tensor):
         # input image should be tensor, so values in range [0,1]
-        number_photons = img / self.pixel_sensitivity
         # fictional number scaling with the number of photons
         # it defines how many photons are necessary to reach a value of 1
         # NOTE: This will be different between images, not just between data sources
-
+        if self.distr is not None:
+            # sample sensitivity from self.distr
+            sensitivity = self.distr.sample()
+            # apply max/min
+            sensitivity = torch.clamp(sensitivity, self.min_sens, self.max_sens)
+        else:
+            sensitivity = self.pixel_sensitivity
+        
+        print(f"Sensitivity: {sensitivity}")
+        number_photons = img / sensitivity
         output = torch.poisson(input=number_photons, generator=self.rng)
-        output = output * self.pixel_sensitivity
+        output = torch.clamp(output * sensitivity, 0.0, 1.0)
         return output
     
