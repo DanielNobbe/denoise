6f3007c673ba6a7306ed3cbda7ebdb8860fbf905
SoerenToennesen <sorentoennesen@gmail.com>
2023-11-11 12:40:05 +0100

    Removed not used functions from homepage

--- webapp/pages/homepage.py
+++ webapp/pages/homepage.py
@@ -1,18 +1,4 @@
 import streamlit as st
-import numpy as np
-import os
-from PIL import Image
-
-def file_selector(folder_path='.'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
-
-def load_image(img):
-    img = img.resize((224, 224))
-    im_array = np.array(img) / 255 # a normalised 2D array
-    im_array = im_array.reshape(-1, 224, 224, 3)   # to shape as (1, 224, 224, 3)
-    return im_array
 
 def homepage():
     st.title("De-noise output")
@@ -24,5 +10,3 @@ def homepage():
             st.image(uploaded_img)
         except:
             st.write("Image in bad format")
-
-
