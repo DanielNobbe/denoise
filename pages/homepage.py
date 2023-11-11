7080a944a9fd61c89d7173372c6d26aae7d67d99
SoerenToennesen <sorentoennesen@gmail.com>
2023-11-11 13:56:50 +0100

    Added img conversion to numpy in homepage

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import imageio.v3 as iio
 
 st.title("De-noise output")
 st.write("""### Upload image to remove noise""") # H3 tag
@@ -7,5 +8,7 @@ if uploaded_img:
     try:
         st.write("Uploaded image:")
         st.image(uploaded_img)
+        img = iio.imread(uploaded_img) # image bytes to numpy array
+        st.write("TODO: Run the image through a model")
     except:
         st.write("Image in bad format")
