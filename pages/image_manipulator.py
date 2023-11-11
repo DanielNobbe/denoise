e9f72b9c96184fa66e5cb6149e08c2f82dbbe156
SoerenToennesen <sorentoennesen@gmail.com>
2023-11-11 13:54:54 +0100

    Added image manipulator (page where you can upload image and add noise to it)

---
+++ pages/image_manipulator.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import imageio.v3 as iio
+import cv2
+from scripts.apply_noise import shot_noise, salt_and_pepper
+
+def add_noise(img):
+    st.write("""### Add noise""") # H3 tag
+    noise_choise = st.selectbox("Choose noise type", options=["Gaussian noise", "Shot noise", "Salt & pepper noise"])
+    if noise_choise == "Gaussian noise":
+        return
+    elif noise_choise == "Shot noise":
+        st.write("Image with shot noise applied:")
+        return shot_noise(img)
+    elif noise_choise == "Salt & pepper noise":
+        st.write("Image with salt & pepper noise applied:")
+        return salt_and_pepper(img, False)
+
+st.title("De-noise image manipulator")
+st.write("""### Upload image to manipulate it with noise""") # H3 tag
+uploaded_img = st.file_uploader("Choose image", type=["jpg", "jpeg", "png"])
+if uploaded_img:
+    try:
+        st.write("Uploaded image:")
+        st.image(uploaded_img, width=200)
+        img = iio.imread(uploaded_img)
+        img_with_noise = add_noise(img)
+        if img_with_noise is not None:
+            image_bytes = cv2.imencode('.jpg', img_with_noise)[1].tobytes()
+            st.image(image_bytes, width=200)
+            st.write("Download image with noise:")
+            st.download_button(label="Download image", data=image_bytes, file_name='img_with_noise.png', mime='image/png')
+    except Exception as e:
+        print(e)
+        st.write(e)
