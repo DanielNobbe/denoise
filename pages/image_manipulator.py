dbb39e17f01762b3ad9cbdad9c159f9666bf2811
SoerenToennesen <sorentoennesen@gmail.com>
2023-11-12 20:19:16 +0100

    Fixed loading images in opencv from BGR to RGB

--- pages/image_manipulator.py
+++ pages/image_manipulator.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import imageio.v3 as iio
 import cv2
+import numpy as np
 from scripts.apply_noise import shot_noise, salt_and_pepper
 
 def add_noise(img):
@@ -13,7 +14,7 @@ def add_noise(img):
         return shot_noise(img)
     elif noise_choise == "Salt & pepper noise":
         st.write("Image with salt & pepper noise applied:")
-        return salt_and_pepper(img, False)
+        return salt_and_pepper(img, True)
 
 st.title("De-noise image manipulator")
 st.write("""### Upload image to manipulate it with noise""") # H3 tag
@@ -21,12 +22,12 @@ uploaded_img = st.file_uploader("Choose image", type=["jpg", "jpeg", "png"])
 if uploaded_img:
     try:
         st.write("Uploaded image:")
-        st.image(uploaded_img, width=200)
+        st.image(uploaded_img, width=300)
         img = iio.imread(uploaded_img)
         img_with_noise = add_noise(img)
         if img_with_noise is not None:
-            image_bytes = cv2.imencode('.jpg', img_with_noise)[1].tobytes()
-            st.image(image_bytes, width=200)
+            image_bytes = cv2.imencode('.jpg', img_with_noise[::-1])[1].tobytes()  # open CV reads as BGR
+            st.image(image_bytes, width=300)
             st.write("Download image with noise:")
             st.download_button(label="Download image", data=image_bytes, file_name='img_with_noise.png', mime='image/png')
     except Exception as e:
