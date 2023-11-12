80a5fcd3b2e09c894a56bed6945a2469808bb4a6
SoerenToennesen <sorentoennesen@gmail.com>
2023-11-12 20:58:11 +0100

    Improved exception handling by being explicit

--- webapp.py
+++ webapp.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import imageio.v3 as iio
-import pickle
+from PIL import UnidentifiedImageError
 
 def load_model():
     return
     # with open('model.pkl', 'rb') as file:
     #     data = pickle.load(file)
     # return data
-data = load_model()
 
 def main():
+    data = load_model()
     st.title("De-noise output")
     st.write("""### Upload image to remove noise""") # H3 tag
     uploaded_img = st.file_uploader("Choose image", type=["jpg", "jpeg", "png"])
@@ -19,8 +19,9 @@ def main():
             st.image(uploaded_img)
             img = iio.imread(uploaded_img) # image bytes to numpy array
             st.write("TODO: Run the image through a model")
-        except:
+        except UnidentifiedImageError as e:
             st.write("Image in bad format")
+            st.write(e)
 
 if __name__ == '__main__':
     main()
