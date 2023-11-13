f588c2c991fea77d0baa6ed4439697bc4100496b
Daniel Nobbe <daniellnobbe@gmail.com>
2023-11-13 19:14:42 +0100

    add inference to webapp

--- webapp.py
+++ webapp.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import imageio.v3 as iio
 from PIL import UnidentifiedImageError
+from PIL import Image
+from scripts.infer import infer
+import cv2
 
-def load_model():
-    return
+# def load_model():
+#     return
     # with open('model.pkl', 'rb') as file:
     #     data = pickle.load(file)
     # return data
 
 def main():
-    data = load_model()
+    # data = load_model()
     st.title("De-noise output")
     st.write("""### Upload image to remove noise""") # H3 tag
     uploaded_img = st.file_uploader("Choose image", type=["jpg", "jpeg", "png"])
@@ -22,6 +25,11 @@ def main():
         except UnidentifiedImageError as e:
             st.write("Image in bad format")
             st.write(e)
+        denoised = infer(Image.fromarray(img))[0].transpose(1, 2, 0) * 255
+        image_bytes = cv2.imencode('.jpg', denoised[:,:,::-1])[1].tobytes()  # open CV reads as BGR
+        st.image(image_bytes)
+        st.write("Download denoised image:")
+        st.download_button(label="Download image", data=image_bytes, file_name='denoised.png', mime='image/png')
 
 if __name__ == '__main__':
     main()
