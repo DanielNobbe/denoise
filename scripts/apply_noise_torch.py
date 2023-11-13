a70336c1d9c9c4d1cffb23e53896f2ebb6169fe3
DanielNobbe <42846160+DanielNobbe@users.noreply.github.com>
2023-11-13 18:45:31 +0100

    cleanup

--- scripts/apply_noise_torch.py
+++ scripts/apply_noise_torch.py
@@ -11,14 +11,12 @@ tensor_transform = tv.transforms.ToTensor()
 pil_transform = tv.transforms.ToPILImage()
 
 shot_noise = ShotNoise(sensitivity=-4.8, sensitivity_sigma=1.2)
-# shot_noise = ShotNoise(sensitivity=0.001)
 
 def main():
     os.makedirs('results', exist_ok=True)
     image_path = './data/div2k/original/val/0801.png'
     img = tensor_transform(Image.open(image_path))
     new_img = shot_noise(img)
-    # new_img = shot_noise(img)
 
     sv_img = pil_transform(new_img)
     sv_img.save("./results/test.png")
