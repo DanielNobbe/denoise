7ce9526e07c73ce55d637dd22ee02f717fa028d0
Daniel Nobbe <daniellnobbe@gmail.com>
2023-12-07 21:26:47 +0100

    add gaussian noise to torch test script

--- scripts/apply_noise_torch.py
+++ scripts/apply_noise_torch.py
@@ -4,19 +4,21 @@ import pdb
 import os
 from PIL import Image
 
-from models.noise import ShotNoise
+from models.noise import ShotNoise, GaussianNoise
 import torchvision as tv
 
 tensor_transform = tv.transforms.ToTensor()
 pil_transform = tv.transforms.ToPILImage()
 
 shot_noise = ShotNoise(sensitivity=-4.8, sensitivity_sigma=1.2)
+gaussian_noise = GaussianNoise(rgb_variance=(0.20,0.20,0.20))
 
 def main():
     os.makedirs('results', exist_ok=True)
     image_path = './data/div2k/original/val/0801.png'
     img = tensor_transform(Image.open(image_path))
-    new_img = shot_noise(img)
+    # new_img = shot_noise(img)
+    new_img = gaussian_noise(img)
 
     sv_img = pil_transform(new_img)
     sv_img.save("./results/test.png")
