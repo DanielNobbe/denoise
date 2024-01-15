65e19bb0a02bbd537ee9680c4f0b9ac24fa3ae91
Daniel Nobbe <daniellnobbe@gmail.com>
2024-01-15 18:29:36 +0100

    apply all noises and realistic params

--- scripts/apply_noise_torch.py
+++ scripts/apply_noise_torch.py
@@ -10,17 +10,18 @@ import torchvision as tv
 tensor_transform = tv.transforms.ToTensor()
 pil_transform = tv.transforms.ToPILImage()
 
-shot_noise = ShotNoise(sensitivity=-4.8, sensitivity_sigma=1.2)
-gaussian_noise = GaussianNoise(rgb_variance=(0.20,0.20,0.20))
-salt_and_pepper_noise = SaltAndPepperNoise(prob_black=0.005, prob_white=0.005, subpixel=False)
+shot_noise = ShotNoise(sensitivity=-10, sensitivity_sigma=0.001)  # negative mu to sample from lognormal
+gaussian_noise = GaussianNoise(rgb_variance=(0.01,0.005,0.005))
+salt_and_pepper_noise = SaltAndPepperNoise(prob_black=0.00005, prob_white=0.00005, subpixel=True)
 
 def main():
     os.makedirs('results', exist_ok=True)
     image_path = './data/div2k/original/val/0801.png'
     img = tensor_transform(Image.open(image_path))
-    # new_img = shot_noise(img)
-    # new_img = gaussian_noise(img)
-    new_img = salt_and_pepper_noise(img)
+    new_img = img
+    new_img = shot_noise(new_img)
+    new_img = gaussian_noise(new_img)
+    # new_img = salt_and_pepper_noise(new_img)
 
     sv_img = pil_transform(new_img)
     sv_img.save("./results/test.png")
