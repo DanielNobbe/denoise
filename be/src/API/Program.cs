55befbcf261c9a823fc5c661e6ab48a0df7feae9
SoerenToennesen <sorentoennesen@gmail.com>
2024-03-04 19:41:41 +0100

    Fixed cors policies

--- be/src/API/Program.cs
+++ be/src/API/Program.cs
@@ -18,6 +18,18 @@ builder.Services.RegisterHttpClients(builder);
 // Register services
 builder.Services.RegisterServices();
 
+var UiCorsPolicy = "_UiOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: UiCorsPolicy, policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:3000",
+            "https://localhost:3000"
+        ).AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,7 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(UiCorsPolicy);
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
