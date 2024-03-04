ca9c14a0967da496af68423284a8300133fc999c
SoerenToennesen <sorentoennesen@gmail.com>
2024-03-04 01:56:39 +0100

    Fixed out dto json serialization

--- be/Application/Models/OutDto/PredictionOutDto.cs
+++ be/Application/Models/OutDto/PredictionOutDto.cs
@@ -1,9 +1,15 @@
-namespace Application.InDto;
+using System.Text.Json.Serialization;
 
-public class PredictionOutDto
+namespace Application.Models.OutDto;
+
+public sealed record PredictionOutDto
 {
-    public string Inputs = String.Empty;
-    public object Parameters = new { };
-    public string Image = String.Empty;
-    public bool EnablePatching = false;
+    [property:JsonPropertyName("inputs")]
+    public string Inputs { get; init; } = String.Empty;
+    [property:JsonPropertyName("parameters")]
+    public object Parameters { get; init; } = new { };
+    [property:JsonPropertyName("image")]
+    public string Image { get; init; } = String.Empty;
+    [property:JsonPropertyName("enable_patching")]
+    public bool EnablePatching { get; init; } = false;
 }
