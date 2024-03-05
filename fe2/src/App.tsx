59ec3165de146e812bb963333bc04e12b7486c5e
SoerenToennesen <sorentoennesen@gmail.com>
2024-03-05 17:49:34 +0100

    Added react project

---
+++ fe2/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+          <a>Test</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
